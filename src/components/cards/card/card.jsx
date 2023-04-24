ac58342feaa5f797cd0d82222270ae42ab2b3d60
CodeyBrodey <102107941+CodeyBrodey@users.noreply.github.com>
2023-04-24 13:27:09 -0300

    Styled cards
    Added styling to country cards and started functionality when clicked upon

--- src/components/cards/card/card.jsx
+++ src/components/cards/card/card.jsx
@@ -1,9 +1,10 @@
 import './style.css';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 
 function Card(props) {
-
+    const { cardOpen, setCardOpen } = useState(false);
     const { name, flags, population, region, capital } = props.country;
 
     Card.propTypes = {
@@ -20,16 +21,25 @@ function Card(props) {
         }).isRequired,
     };
 
+    function handleClick() {
+        setCardOpen(true);
+        console.log(cardOpen)
+    }
+
 
     return(
-        <div className="card">
+        <div className="card" onClick={ handleClick }>
             <img src={ flags.png } alt="" className='card__img'/>
+
             <div className="card__info">
                 <h3 className='card__h3'>{ name.common }</h3>
-                <p className='card__population'>Population: <span className='card__population--result'>{ population.toLocaleString() }</span></p>
-                <p className='card__region'>Region: <span className='card__region--result'>{ region }</span></p>
-                <p className='capital'>Capital: <span className='card__capital--result'>{ capital }</span></p>
+                <div className="card__info__details">
+                    <p className='card__population'>Population: <span className='card__population--result'>{ population.toLocaleString() }</span></p>
+                    <p className='card__region'>Region: <span className='card__region--result'>{ region }</span></p>
+                    <p className='capital'>Capital: <span className='card__capital--result'>{ capital }</span></p>
+                </div>
             </div>
+
         </div>
     )
 
