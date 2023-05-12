97e282c19ba03a8a23a815e883b6cf5a300f289b
CodeyBrodey <102107941+CodeyBrodey@users.noreply.github.com>
2023-05-12 01:27:28 -0300

    Finished media queries

--- src/components/CardDisplay/cardDisplay.jsx
+++ src/components/CardDisplay/cardDisplay.jsx
@@ -46,77 +46,79 @@ function CardDisplay(props) {
 
     return(
         <>
-            <div className={ darkModeActive ? 'search-bar--dark' : "search-bar" }>
-                <svg className={ darkModeActive ? 'search-bar__icon--dark' : 'search-bar__icon' } xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <div className='CardDisplay-container'>
+                <div className={ darkModeActive ? 'search-bar--dark' : "search-bar" }>
+                    <svg className={ darkModeActive ? 'search-bar__icon--dark' : 'search-bar__icon' } xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
-                </svg>
+                    </svg>
 
-                <input className={ darkModeActive ? 'search-bar__input--dark' : 'search-bar__input' } type="text" placeholder='Search for a country...' onChange={ handleSearchInput } />
-            </div>
-
-            <button className={ darkModeActive ? 'filter--dark' : 'filter' } onClick={ handleFilterClick }>
-                <span className='filter__text'>{ filterOptionStatus === null ? 'Filter by Region' : filterOptionStatus }</span>
-
-                <svg className={ darkModeActive ? 'filter__icon--dark' : 'filter__icon' } xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
-                </svg>
-
-                <div className={ darkModeActive ? 
-                    filterOpen ? 'filter__options filter__options--open--dark' : 'filter__options' 
-                    : 
-                    filterOpen ? 'filter__options filter__options--open' : 'filter__options' }>
-                    <span className={darkModeActive ? 
-                        filterOptionStatus === 'Africa' ? 
-                        'filter__options__btns--dark--active filter__options__btns--dark' :
-                        'filter__options__btns--dark'
-                        : 
-                        filterOptionStatus === 'Africa' ? 
-                        'filter__options__btns--active filter__options__btns' :
-                        'filter__options__btns'
-                        } onClick={handleFilterOption}>Africa</span>
-
-                    <span className={darkModeActive ? 
-                        filterOptionStatus === 'Americas' ? 
-                        'filter__options__btns--dark--active filter__options__btns--dark' :
-                        'filter__options__btns--dark'
-                        : 
-                        filterOptionStatus === 'Americas' ? 
-                        'filter__options__btns--active filter__options__btns' :
-                        'filter__options__btns'
-                        } onClick={handleFilterOption}>Americas</span>
-
-                    <span className={darkModeActive ? 
-                        filterOptionStatus === 'Asia' ? 
-                        'filter__options__btns--dark--active filter__options__btns--dark' :
-                        'filter__options__btns--dark'
-                        : 
-                        filterOptionStatus === 'Asia' ? 
-                        'filter__options__btns--active filter__options__btns' :
-                        'filter__options__btns'
-                        } onClick={handleFilterOption}>Asia</span>
-
-                    <span className={darkModeActive ? 
-                        filterOptionStatus === 'Europe' ? 
-                        'filter__options__btns--dark--active filter__options__btns--dark' :
-                        'filter__options__btns--dark'
-                        : 
-                        filterOptionStatus === 'Europe' ? 
-                        'filter__options__btns--active filter__options__btns' :
-                        'filter__options__btns'
-                        } onClick={handleFilterOption}>Europe</span>
-                        
-                    <span className={darkModeActive ? 
-                        filterOptionStatus === 'Oceania' ? 
-                        'filter__options__btns--dark--active filter__options__btns--dark' :
-                        'filter__options__btns--dark'
-                        : 
-                        filterOptionStatus === 'Oceania' ? 
-                        'filter__options__btns--active filter__options__btns' :
-                        'filter__options__btns'
-                        } onClick={handleFilterOption}>Oceania</span>
+                    <input className={ darkModeActive ? 'search-bar__input--dark' : 'search-bar__input' } type="text" placeholder='Search for a country...' onChange={ handleSearchInput } />
                 </div>
-            </button>
 
+                <button className={ darkModeActive ? 'filter--dark' : 'filter' } onClick={ handleFilterClick }>
+                    <span className='filter__text'>{ filterOptionStatus === null ? 'Filter by Region' : filterOptionStatus }</span>
+
+                    <svg className={ darkModeActive ? 'filter__icon--dark' : 'filter__icon' } xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M201.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 338.7 54.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/>
+                    </svg>
+
+                    <div className={ darkModeActive ? 
+                        filterOpen ? 'filter__options filter__options--open--dark' : 'filter__options' 
+                        : 
+                        filterOpen ? 'filter__options filter__options--open' : 'filter__options' }>
+                        <span className={darkModeActive ? 
+                        filterOptionStatus === 'Africa' ? 
+                        'filter__options__btns--dark--active filter__options__btns--dark' :
+                        'filter__options__btns--dark'
+                        : 
+                        filterOptionStatus === 'Africa' ? 
+                        'filter__options__btns--active filter__options__btns' :
+                        'filter__options__btns'
+                            } onClick={handleFilterOption}>Africa</span>
+
+                        <span className={darkModeActive ? 
+                        filterOptionStatus === 'Americas' ? 
+                        'filter__options__btns--dark--active filter__options__btns--dark' :
+                        'filter__options__btns--dark'
+                        : 
+                        filterOptionStatus === 'Americas' ? 
+                        'filter__options__btns--active filter__options__btns' :
+                        'filter__options__btns'
+                            } onClick={handleFilterOption}>Americas</span>
+
+                        <span className={darkModeActive ? 
+                        filterOptionStatus === 'Asia' ? 
+                        'filter__options__btns--dark--active filter__options__btns--dark' :
+                        'filter__options__btns--dark'
+                        : 
+                        filterOptionStatus === 'Asia' ? 
+                        'filter__options__btns--active filter__options__btns' :
+                        'filter__options__btns'
+                            } onClick={handleFilterOption}>Asia</span>
+
+                        <span className={darkModeActive ? 
+                        filterOptionStatus === 'Europe' ? 
+                        'filter__options__btns--dark--active filter__options__btns--dark' :
+                        'filter__options__btns--dark'
+                        : 
+                        filterOptionStatus === 'Europe' ? 
+                        'filter__options__btns--active filter__options__btns' :
+                        'filter__options__btns'
+                            } onClick={handleFilterOption}>Europe</span>
+                        
+                        <span className={darkModeActive ? 
+                        filterOptionStatus === 'Oceania' ? 
+                        'filter__options__btns--dark--active filter__options__btns--dark' :
+                        'filter__options__btns--dark'
+                        : 
+                        filterOptionStatus === 'Oceania' ? 
+                        'filter__options__btns--active filter__options__btns' :
+                        'filter__options__btns'
+                            } onClick={handleFilterOption}>Oceania</span>
+                    </div>
+                </button>
+            </div>
+            
             <div className='country-cards'>
                 {countryCards.filter((value) => {
                     if(value.name.common.toLowerCase().includes(searchQuery) && value.region === filterOptionStatus)return value
--- src/components/CardDisplay/cards/card.jsx
+++ src/components/CardDisplay/cards/card.jsx
@@ -109,72 +109,77 @@ function Card(props) {
     if(cardOpen === true) {
         return(
             <div className={ darkMode ? 'card--open--dark' : 'card--open' }>
-                <button className={ darkMode ? 'card--open__button--dark' : 'card--open__button' } onClick={ handleBackClick }> 
+                <button className={ darkMode ? 'card--open__button--dark card--open__button__back' : 'card--open__button card--open__button__back' } onClick={ handleBackClick }> 
                     <svg className='card--open__button__icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288 480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128z"/>
                     </svg> Back
                 </button>
+                    
+                <div className="country-container">
+                    <img className='card--open__img' src={ borderCountry ? borderCountry.flags.png : flags.png } alt="" />
 
-                <img className='card--open__img' src={ borderCountry ? borderCountry.flags.png : flags.png } alt="" />
+                    <div className='card--open__info'>
+                        <h2>{ borderCountry ? borderCountry.name.common : name.common }</h2>
 
-                <div className='card--open__info'>
-                    <h2>{ borderCountry ? borderCountry.name.common : name.common }</h2>
+                        <div className="detail-container">
+                            <div className='card--open__info__details'>
+                                <p className='card__native'>Native Name: <span className='card__native--result'>{ officialNativeName }</span></p>
 
-                    <div className='card--open__info__details'>
-                        <p className='card__native'>Native Name: <span className='card__native--result'>{ officialNativeName }</span></p>
+                                <p className='card__population'>Population: <span className='card__population--result'>{ borderCountry ? borderCountry.population.toLocaleString() : population.toLocaleString() }</span></p>
 
-                        <p className='card__population'>Population: <span className='card__population--result'>{ borderCountry ? borderCountry.population.toLocaleString() : population.toLocaleString() }</span></p>
+                                <p className='card__region'>Region: <span className='card__region--result'>{ borderCountry ? borderCountry.region : region }</span></p>
 
-                        <p className='card__region'>Region: <span className='card__region--result'>{ borderCountry ? borderCountry.region : region }</span></p>
+                                <p className='card__subregion'>Sub Region: <span className='card__subregion--result'>{ borderCountry ? borderCountry.subregion : subregion }</span></p>
 
-                        <p className='card__subregion'>Sub Region: <span className='card__subregion--result'>{ borderCountry ? borderCountry.subregion : subregion }</span></p>
+                                <p className='card__capital'>Capital: <span className='card__capital--result'>{ borderCountry ? borderCountry.capital : capital }</span></p>
+                            </div>
 
-                        <p className='card__capital'>Capital: <span className='card__capital--result'>{ borderCountry ? borderCountry.capital : capital }</span></p>
-                    </div>
+                            <div className='card--open__info__technical'>
+                                <p className='card__domain'>Top Level Domain: <span className='card__domain--result'>{ borderCountry ? borderCountry.tld[0] : tld[0] }</span></p>
 
-                    <div className='card--open__info__technical'>
-                        <p className='card__domain'>Top Level Domain: <span className='card__domain--result'>{ borderCountry ? borderCountry.tld[0] : tld[0] }</span></p>
+                                <p className='card__currencies'>Currencies: <span className='card__currencies--result'>{ currency }</span></p>
 
-                        <p className='card__currencies'>Currencies: <span className='card__currencies--result'>{ currency }</span></p>
-
-                        <p className='card__languages'>Languages: <span className='card__languages--result'>{ language }</span></p>
-                    </div>
-
-                    {borders ?
-                        <div className='card--open__info__borders'>
-                            <h3>Border Countries:</h3>
-
-                            <div className="card--open__info__borders__buttons">
-                                {borderCountry && borders ?
-                                borderCountry.borders.map(border => {
-                                    const borderCountry = countryCards.find(c => c.cca3 === border);
-                                    function handleBorderClick() {
-                                        setborderCountry(borderCountry)
-                                    }
-
-                                    return (
-                                        <button className={darkMode ? 'card--open__button--dark' : 'card--open__button'} key={border} onClick={handleBorderClick}>
-                                            {borderCountry.name.common}
-                                        </button>
-                                    )
-                                }) :
-                                borders.map(border => {
-                                    const borderCountry = countryCards.find(c => c.cca3 === border);
-                                    function handleBorderClick() {
-                                        setborderCountry(borderCountry)
-                                    }
-
-                                    return (
-                                        <button className={darkMode ? 'card--open__button--dark' : 'card--open__button'} key={border} onClick={handleBorderClick}>
-                                            {borderCountry.name.common}
-                                        </button>
-                                    )
-                                })}
+                                <p className='card__languages'>Languages: <span className='card__languages--result'>{ language }</span></p>
                             </div>
                         </div>
-                        :
-                        null
-                    }
+                    
+
+                        {borders ?
+                            <div className='card--open__info__borders'>
+                                <h3>Border Countries:</h3>
+
+                                <div className="card--open__info__borders__buttons">
+                                    {borderCountry && borders ?
+                                    borderCountry.borders.map(border => {
+                                        const borderCountry = countryCards.find(c => c.cca3 === border);
+                                        function handleBorderClick() {
+                                            setborderCountry(borderCountry)
+                                        }
+
+                                        return (
+                                            <button className={darkMode ? 'card--open__button--dark' : 'card--open__button'} key={border} onClick={handleBorderClick}>
+                                                {borderCountry.name.common}
+                                            </button>
+                                        )
+                                    }) :
+                                    borders.map(border => {
+                                        const borderCountry = countryCards.find(c => c.cca3 === border);
+                                        function handleBorderClick() {
+                                            setborderCountry(borderCountry)
+                                        }
+
+                                        return (
+                                            <button className={darkMode ? 'card--open__button--dark' : 'card--open__button'} key={border} onClick={handleBorderClick}>
+                                                {borderCountry.name.common}
+                                            </button>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                            :
+                            null
+                        }
+                    </div>
                 </div>
             </div>
         )
