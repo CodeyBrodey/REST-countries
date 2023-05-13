69f434ea166eaf75648ddb850037ce1a1a43a07b
CodeyBrodey <102107941+CodeyBrodey@users.noreply.github.com>
2023-05-13 16:58:01 -0300

    Correction of last update
    Correction of search mobile fix

--- src/components/CardDisplay/CardDisplay.jsx
+++ src/components/CardDisplay/CardDisplay.jsx
@@ -52,10 +52,10 @@ function CardDisplay(props) {
                     <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                     </svg>
 
-                    <input className={ darkModeActive ? 'search-bar__input--dark' : 'search-bar__input' } type="text" placeholder='Search for a country...' onChange={ handleSearchInput } />
+                    <input className={ darkModeActive ? 'search-bar__input--dark' : 'search-bar__input' } type="text" placeholder='Search for a country...' onKeyDown={ handleSearchInput } />
                 </div>
 
-                <button className={ darkModeActive ? 'filter--dark' : 'filter' } onKeyDown={ handleFilterClick }>
+                <button className={ darkModeActive ? 'filter--dark' : 'filter' } onClick={ handleFilterClick }>
                     <span className='filter__text'>{ filterOptionStatus === null ? 'Filter by Region' : filterOptionStatus }</span>
 
                     <svg className={ darkModeActive ? 'filter__icon--dark' : 'filter__icon' } xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
